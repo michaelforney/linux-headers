8907e151c25fdaac10361ec396c6fe71edbeb9a7
Geert Uytterhoeven <geert@linux-m68k.org>
2013-10-02 11:37:33 +0200

    m68k/UAPI: Disintegrate arch/m68k/include/asm/bootinfo.h
    Export the bootinfo definitions that are used by bootstrap loaders, and
    split them up in generic and platform-specific parts.
    
    Signed-off-by: Geert Uytterhoeven <geert@linux-m68k.org>

---
+++ arch/m68k/asm/bootinfo-apollo.h
@@ -0,0 +1,16 @@
+/*
+** asm/bootinfo-apollo.h -- Apollo-specific boot information definitions
+*/
+
+#ifndef _ASM_M68K_BOOTINFO_APOLLO_H
+#define _ASM_M68K_BOOTINFO_APOLLO_H
+
+
+    /*
+     *  Apollo-specific tags
+     */
+
+#define BI_APOLLO_MODEL		0x8000	/* model (u_long) */
+
+
+#endif /* _ASM_M68K_BOOTINFO_APOLLO_H */

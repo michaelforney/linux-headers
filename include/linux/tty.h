2d874ee6d1aeed96ab938b1c7a98bbbf980cb4d2
Okash Khawaja <okash.khawaja@gmail.com>
2017-05-15 18:45:33 +0100

    staging: speakup: add tty-based comms functions
    This adds spk_ttyio.c file. It contains a set of functions which implement
    those methods in spk_synth struct which relate to sending bytes out using
    serial comms. Implementations in this file perform the same function but
    using TTY subsystem instead. Currently synths access serial ports, directly
    poking standard ISA ports by trying to steal them from serial driver. Some ISA
    cards actually need this way of doing it, but most other synthesizers don't,
    and can actually work by using the proper TTY subsystem through a new N_SPEAKUP
    line discipline. So this adds the methods for drivers to switch to accessing
    serial ports through the TTY subsystem, whenever appropriate.
    
    Signed-off-by: Okash Khawaja <okash.khawaja@gmail.com>
    Reviewed-by: Samuel Thibault <samuel.thibault@ens-lyon.org>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- include/linux/tty.h
+++ include/linux/tty.h
@@ -35,5 +35,6 @@
 #define N_TRACESINK	23	/* Trace data routing for MIPI P1149.7 */
 #define N_TRACEROUTER	24	/* Trace data routing for MIPI P1149.7 */
 #define N_NCI		25	/* NFC NCI UART */
+#define N_SPEAKUP	26	/* Speakup communication with synths */
 
 #endif /* _LINUX_TTY_H */

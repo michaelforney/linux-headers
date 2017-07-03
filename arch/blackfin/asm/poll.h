6d688905246e731196001fcabd99e674d1fe4667
Al Viro <viro@zeniv.linux.org.uk>
2017-07-02 22:05:03 -0400

    define __poll_t, annotate constants
    Signed-off-by: Al Viro <viro@zeniv.linux.org.uk>

--- arch/blackfin/asm/poll.h
+++ arch/blackfin/asm/poll.h
@@ -9,8 +9,8 @@
 #ifndef __BFIN_POLL_H
 #define __BFIN_POLL_H
 
-#define POLLWRNORM	4 /* POLLOUT */
-#define POLLWRBAND	256
+#define POLLWRNORM	(__poll_t)4 /* POLLOUT */
+#define POLLWRBAND	(__poll_t)256
 
 #include <asm-generic/poll.h>
 

24885fef57cd51bc9de95d29d1c52c10393bb9b8
Firoz Khan <firoz.khan@linaro.org>
2018-09-26 11:06:25 +0530

    parisc: add __NR_syscalls along with __NR_Linux_syscalls
    __NR_Linux_syscalls macro holds the number of system call
    exist in parisc architecture. We have to change the value
    of __NR_Linux_syscalls, if we add or delete a system call.
    
    One of the patch in this patch series has a script which
    will generate a uapi header based on syscall.tbl file.
    The syscall.tbl file contains the total number of system
    calls information. So we have two option to update __NR-
    _Linux_syscalls value.
    
    1. Update __NR_Linux_syscalls in asm/unistd.h manually by
       counting the no.of system calls. No need to update __NR-
       _Linux_syscalls until we either add a new system call or
       delete existing system call.
    
    2. We can keep this feature it above mentioned script,
       that will count the number of syscalls and keep it in
       a generated file. In this case we don't need to expli-
       citly update __NR_Linux_syscalls in asm/unistd.h file.
    
    The 2nd option will be the recommended one. For that, I
    added the __NR_syscalls macro in uapi/asm/unistd.h along
    with __NR_Linux_syscalls asm/unistd.h. The macro __NR_sys-
    calls also added for making the name convention same across
    all architecture. While __NR_syscalls isn't strictly part
    of the uapi, having it as part of the generated header to
    simplifies the implementation. We also need to enclose
    this macro with #ifdef __KERNEL__ to avoid side effects.
    
    Signed-off-by: Firoz Khan <firoz.khan@linaro.org>
    Acked-by: Helge Deller <deller@gmx.de>
    Signed-off-by: Helge Deller <deller@gmx.de>

--- arch/parisc/asm/unistd.h
+++ arch/parisc/asm/unistd.h
@@ -366,8 +366,7 @@
 #define __NR_statx		(__NR_Linux + 349)
 #define __NR_io_pgetevents	(__NR_Linux + 350)
 
-#define __NR_Linux_syscalls	(__NR_io_pgetevents + 1)
 
-#define LINUX_GATEWAY_ADDR      0x100
+#define LINUX_GATEWAY_ADDR	0x100
 
 #endif /* _ASM_PARISC_UNISTD_H_ */
